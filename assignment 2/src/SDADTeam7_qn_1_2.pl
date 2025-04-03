854d7befe4e6f6d7440e167a070bdd2d9ca46e12
williamfaraday123 <williamfaraday123@gmail.com>
2025-04-03 10:38:29 +0800

    completed assignment 2

---
+++ assignment 2/src/SDADTeam7_qn_1_2.pl
@@ -0,0 +1,5 @@
+﻿competitor(sumsum, appy).
+developed(sumsum, galactica_s3).
+stolen(stevey, galactica_s3).
+boss(stevey, appy).
+unethical(X) :- boss(X, Company), competitor(Competitor, Company), developed(Competitor, Technology), stolen(X, Technology).
